3c530b912e31b9fdf6a2ab4d54157db76d14d27d
Pu Wang <puwang@thoughtworks.com>
2017-09-09 22:08:03 +0800

    format

--- src/main/java/Print.java
+++ src/main/java/Print.java
@@ -1,18 +1,31 @@
 public class Print {
 
     String printAsterisk(int n, String type) {
-        StringBuilder result = new StringBuilder();
         if ("horizontal".equals(type)) {
-            for (int index = 0; index < n; index++) {
-                result.append("*");
-            }
+            return print(n, "*");
         } else if ("vertical".equals(type)) {
-            for (int index = 0; index < n; index++) {
-                result.append("*\n");
-            }
-            result.replace(result.length() - 1, result.length(), "");
+            return print(n, "*\n");
+        }
+        return null;
+    }
+
+    private String print(int n,  String symbol) {
+        StringBuilder result = new StringBuilder();
+        for (int index = 0; index < n; index++) {
+            result.append(symbol);
+        }
+        if (hasBreakLineInEnd(result)) {
+            removeBreakLine(result);
         }
         System.out.println(result);
         return result.toString();
+    }
+
+    private StringBuilder removeBreakLine(StringBuilder result) {
+        return result.replace(result.length() - 1, result.length(), "");
+    }
+
+    private boolean hasBreakLineInEnd(StringBuilder result) {
+        return result.lastIndexOf("\n") == result.length() - 1;
     }
 }
