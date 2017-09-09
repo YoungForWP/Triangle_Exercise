2fce519355f1e289f9f8c362647a00021b418430
Pu Wang <puwang@thoughtworks.com>
2017-09-09 21:38:57 +0800

    support for two vertical asteris

--- src/main/java/Print.java
+++ src/main/java/Print.java
@@ -1,10 +1,15 @@
 public class Print {
 
-    String printAsterisk(int n) {
+    String printAsterisk(int n, String type) {
         StringBuilder result = new StringBuilder();
-        for (int index = 0; index < n; index++) {
-            result.append("*");
+        if ("horizontal".equals(type)) {
+            for (int index = 0; index < n; index++) {
+                result.append("*");
+            }
+        } else if ("vertical".equals(type)) {
+            result.append("*\n*");
         }
+        System.out.println(result);
         return result.toString();
     }
 }
--- src/test/java/TriangleTest.java
+++ src/test/java/TriangleTest.java
@@ -7,11 +7,16 @@ public class TriangleTest {
 
     @Test
     public void output_asterisk_when_input_one() throws Exception {
-        assertEquals("*", print.printAsterisk(1));
+        assertEquals("*", print.printAsterisk(1, "horizontal"));
     }
 
     @Test
     public void output_multiple_asterisk() throws Exception {
-        assertEquals("********", print.printAsterisk(8));
+        assertEquals("********", print.printAsterisk(8, "horizontal"));
+    }
+
+    @Test
+    public void output_vertical_asterisk() throws Exception {
+        assertEquals("*\n*", print.printAsterisk(2, "vertical"));
     }
 }
